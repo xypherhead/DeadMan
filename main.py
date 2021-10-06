fc23286d2a47dc8f4676004505ea6e1b914b276d
xypherhead <klakade25@gmail.com>
2021-10-07 04:51:29 +0530

    Added .env for TOKEN

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import discord
 from discord.ext import commands
-# from discord.ext.commands.core import command
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
 
 bot = commands.Bot(command_prefix='-')
 
 @bot.event
 async def on_ready():
-    print("We have logged in as:", bot.user.name, bot.user.avatar)
+    print("We have logged in as:", bot.user.name, '#', bot.user.discriminator)
 
 @bot.command()
 async def hello(ctx):
     await ctx.reply("Hello mate!")
 
-bot.run('ODMyMzAyNDUzNTE2OTI2OTk3.YHh0EA.m14rpj80v7G0W7-7LKnXA9V78ls')
+bot.run(getenv('TOKEN'))
