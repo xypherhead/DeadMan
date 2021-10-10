c70dda8e67a9d01347c2e386af72e23dc6f74fb4
xypherhead <klakade25@gmail.com>
2021-10-11 04:44:52 +0530

    added listall and courses commands and  argument-error handling for add command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import json
 from discord.ext import commands
+from discord.ext.commands.errors import BadArgument
+from discord.ext.commands.help import MinimalHelpCommand
 from dotenv import load_dotenv
 from os import getenv
 load_dotenv()
@@ -10,13 +12,12 @@ def DeadMan(file, data):
         courses = data['courses'] = []
     else:
         courses = data['courses']
-    print(data)
 
-    bot = commands.Bot(command_prefix='-')
+    bot = commands.Bot(command_prefix='-', activity = discord.Activity(type = discord.ActivityType.custom) , status = discord.Status.idle, help_command=MinimalHelpCommand())
 
     @bot.event
     async def on_ready():
-        print("We have logged in as:", bot.user.name, '#', bot.user.discriminator)
+        print("We have logged in as:", bot.user.name, '#{}'.format(bot.user.discriminator))
 
     @bot.command()
     async def hello(ctx):
@@ -53,6 +54,43 @@ def DeadMan(file, data):
 
         except Exception as e:
             await ctx.reply(e)
+
+    @add_deadline.error
+    async def bad_argument(ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Wrong argument passed!\nUsage: -add <Course Name> <Deadline Date in DDMM> <Deadline Description>")
+
+    # @bot.command(name='courses')
+    # async def list_courses(ctx):
+    #     try:
+    #         course_list = '**The courses added so far are:**\n>>> '
+    #         for course in courses:
+    #             course_list += course + '\n'
+    #         await ctx.send(course_list)
+    #     except Exception as e:
+    #         await ctx.reply(e)
+
+    @bot.command(name='listall')
+    async def list_deadlines(ctx):
+        try:
+            all_list = '**Deadlines for all courses:**\n'
+            for course in courses:
+                course_list = '`{}:`\n'.format(course)
+                for dl in data[course]:
+                    if(dl == "course_id" or dl == "count"):
+                        continue
+                    else:
+                        course_list += '> '
+                        course_list += str(data[course][dl]["Date"] // 100) + '/' + (str(data[course][dl]["Date"] % 100))
+                        course_list += ': ' + data[course][dl]["Description"] + '\n'
+                all_list += course_list
+            await ctx.send(all_list)
+        except Exception as e:
+            await ctx.reply(e)
+
+    # @bot.command(name='help')
+    # async def _help(ctx):
+    #     pass
 
     bot.run(getenv('TOKEN'))
 
